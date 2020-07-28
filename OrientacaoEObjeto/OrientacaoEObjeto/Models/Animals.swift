61f237c1e15fa7fba6dfc17caaf5f79ee0877e41
Vitor Gomes <vitorminozzi9@gmail.com>
2020-07-27 22:03:04 -0300

    Meus Arquivos que serao utilizados

---
+++ OrientacaoEObjeto/OrientacaoEObjeto/Models/Animals.swift
@@ -0,0 +1,44 @@
+//
+//  Animals.swift
+//  OrientacaoEObjeto
+//
+//  Created by Vitor Gomes on 27/07/20.
+//  Copyright © 2020 Vitor Gomes. All rights reserved.
+//
+
+import Foundation
+
+class Animal {
+    
+    var tamanho: String?
+    var raca: String?
+    var cor: String?
+    var qtdPatas: Int?
+    var genero: String?
+    var especie: String?
+    
+    init(tamanho: String, raca: String, cor: String, qtdPatas: Int, genero: String, especie: String  ){
+        
+        self.tamanho = tamanho
+        self.raca = raca
+        self.cor = cor
+        self.qtdPatas = qtdPatas
+        self.genero = genero
+        self.especie = especie
+        
+    }
+}
+
+
+
+
+struct Animal2 {
+
+    
+    var tamanho: String?
+    var raca: String?
+    var cor: String?
+    var qtdPatas: Int?
+    var genero: String?
+    var especie: String?
+}
---
+++ OrientacaoEObjeto/OrientacaoEObjeto/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  OrientacaoEObjeto
+//
+//  Created by Vitor Gomes on 27/07/20.
+//  Copyright © 2020 Vitor Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+
+    override func viewDidLoad() {
+        
+        var animal: Animal = Animal(tamanho: "Grande", raca: "Husky", cor: "Branco", qtdPatas: 4, genero: "Macho", especie: "Canino")
+        
+        var animal2: Animal2 = Animal2(tamanho: "Pequeno", raca: "Basset", cor: "Marrom", qtdPatas: 4, genero: "Femea", especie: "Canino")
+        
+        
+        
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
