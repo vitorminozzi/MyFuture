9fd574fcc8d568a0eebb6276817c860b346267d8
Vitor Gomes <vitorminozzi9@gmail.com>
2020-07-30 19:38:00 -0300

    Estudo realizado em 30/07 - onde tentei um pouco de classes e layout do app.

---
+++ OrientacaoEObjeto/OrientacaoEObjeto/Models/Cachorro.swift
@@ -0,0 +1,21 @@
+//
+//  Cachorro.swift
+//  OrientacaoEObjeto
+//
+//  Created by Vitor Gomes on 30/07/20.
+//  Copyright © 2020 Vitor Gomes. All rights reserved.
+//
+
+import UIKit
+
+class Cachorro: Animal {
+    
+    override init(tamanho: String, raca: String, cor: String, qtdPatas: Int, genero: String, especie: String) {
+        
+        super.init(tamanho: "Medio", raca: "Husky", cor: "Varias", qtdPatas: 4, genero: "Ambos", especie: "Canino")
+        
+    }
+    
+    
+
+}
--- OrientacaoEObjeto/OrientacaoEObjeto/ViewController.swift
+++ OrientacaoEObjeto/OrientacaoEObjeto/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    var troca = false
+    var jake = ""
     var passaro:Passaro = Passaro(tamanho: "", raca: "", cor: "", qtdPatas: 0, genero: "", especie: "")
+    var husky = Cachorro(tamanho: "", raca: "", cor: "", qtdPatas: 0, genero: "", especie: "")
     
+   	
+   
     
     func apresentarPassaro(){
         
@@ -21,17 +25,53 @@ class ViewController: UIViewController {
     }
     
     
-    
+    func apresentarCachorro() {
+        
+       var iAmJake = "Oi sou o Jake, sou um \(husky.raca)"
+       print(iAmJake)
+       jake = iAmJake
+        
 
+    }
+
+    
     override func viewDidLoad() {
         
+        self.myFriendLabel.layer.borderColor = UIColor.darkGray.cgColor
+        self.myFriendLabel.layer.cornerRadius = 5
+        self.myFriendLabel.layer.borderWidth = 5
+        
         self.apresentarPassaro()
+        print("===========")
+        self.apresentarCachorro()
+       
+     
+      
+    
         
         
     super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
 
-
+    @IBOutlet weak var myFriendLabel: UILabel!
+    
+    @IBAction func friendButton(_ sender: Any) {
+        
+        if troca == false {
+            	
+            self.myFriendLabel.text = jake
+            troca = true
+            
+        }else {
+            
+            self.myFriendLabel.text = "My Friend"
+            troca = false
+        }
+         
+    }
 }
 
