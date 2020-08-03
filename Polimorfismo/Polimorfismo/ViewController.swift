648cc8cd647dcabcd1b3f4c9ed632e4b756f8b9d
Vitor Gomes <vitorminozzi9@gmail.com>
2020-08-03 20:58:24 -0300

    no message

--- Polimorfismo/Polimorfismo/ViewController.swift
+++ Polimorfismo/Polimorfismo/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  Polimorfismo
+//  Animais
 //
-//  Created by Vitor Gomes on 03/08/20.
-//  Copyright © 2020 Vitor Gomes. All rights reserved.
+//  Created by Felipe Miranda on 03/08/20.
+//  Copyright © 2020 Digital House. All rights reserved.
 //
 
 import UIKit
@@ -12,9 +12,87 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var cat1: Animal = Cat(name: "gato1", color: "preto")
+        print(cat1.sendSound())
+        cat1.eat()
+        
+        var cow1: Animal = Cow(name: "vaca1", color: "branco", literMilk: 4)
+        print(cow1.sendSound())
+        cow1.eat()
         // Do any additional setup after loading the view.
+        
+    
+        
+        
     }
 
 
+
+class Cat: Animal {
+
+    override    init(name: String?, color: String?) {
+        
+        super.init(name: name, color: color)
+        
+    }
+    
+    override func sendSound() -> String {
+    
+        return "Miaaaaaaau"
+    }
+    
+    override func eat() {
+        print("Devorando um peixe")
+    }
 }
+
+class Cow: Animal {
+    
+    var literMilk: Int?
+    
+    init(name: String?, color: String?, literMilk: Int? ) {
+        
+        super.init(name: name, color: color)
+        self.literMilk = literMilk
+    }
+    
+    
+    override func sendSound() -> String {
+        return "Muuuuuuuu"
+    }
+    
+    override func eat() {
+        print("Devorando um capim")
+    }
+}
+
+
+class Animal {
+    
+    var name: String?
+    var color: String?
+    
+    init(name: String?, color: String?) {
+        
+        self.name = name
+        self.color = color
+    }
+
+    func sendSound() -> String {
+        return "O animal está emitindo um som"
+    }
+    
+    func eat() {
+        print("O animal esta comendo")
+    }
+    
+    
+    
+    
+
+}
+
+}
+
 
